a2e17d724ee68a7050bc46ef79623aea84838481
Andrey Tutin <and.tutin@gmail.com>
2021-03-14 00:29:16 +0300

    add cors support

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL || 5000;
 const app = express();
 
+app.use(cors());
 app.use(express.json({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/category", require("./routes/category.routes"));
