2f0a7aff81751b98ee18b7b98a86f109978d5213
Andrey Tutin <and.tutin@gmail.com>
2021-03-14 15:55:08 +0300

    login fix

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
 const auth = require("../middleware/auth.middleware");
+const jwt = require('jsonwebtoken');
 const router = Router();
 
 router.post(
@@ -54,30 +55,30 @@ router.post(
   async (req, res) => {
     try {
       const errors = validationResult(req);
-
+      
       if (!errors.isEmpty()) {
         return res.status(400).json({
           message: "Некорректные данные при попытке входа",
         });
       }
-
+      
       const { nickname, password } = req.body;
       const user = await User.findOne({ nickname });
-
+      
       if (!user) {
         return res.status(400).json({ message: "Ошибка авторизации" });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
-
+      
       if (!isMatch) {
         return res.status(400).json({ message: "Ошибка авторизации" });
       }
 
-      const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
         expiresIn: "1h",
       });
-
+      
       res.status(200).json({ token, userId: user.id });
     } catch (error) {
       res
